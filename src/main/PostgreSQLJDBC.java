2a6106f0cb9620262c655875daf264346461da98
Dong Chen <dong@spatial-2x.ist.psu.edu>
2013-08-21 21:59:03 -0400

    add additional relationship tables

--- src/main/PostgreSQLJDBC.java
+++ src/main/PostgreSQLJDBC.java
@@ -17,7 +17,7 @@ import java.util.Map;
 import org.postgresql.geometric.PGpoint;
 
 public class PostgreSQLJDBC {
-	List<String> tables = Arrays.asList("person", "event", "location", "facility", "vehicle", "organization", "event_person", "event_location", "event_organization", "event_facility", "event_vehicle", "person_organization");
+	List<String> tables = Arrays.asList("person", "event", "location", "facility", "vehicle", "organization", "event_person", "event_location", "event_organization", "event_facility", "event_vehicle", "person_organization", "event_event", "facility_facility", "facility_location", "facility_person", "facility_organization", "facility_vehicle", "location_location", "location_person", "location_organization", "location_vehicle", "person_person", "person_vehicle", "organization_organization", "organization_vehicle", "vehicle_vehicle");
 	   
 	
    public static void main(String args[]) {
@@ -79,7 +79,25 @@ public class PostgreSQLJDBC {
    
    public void dropTables(Connection c)
    {
+	   List<String> toDropTables = new ArrayList<String>();
 	   System.out.println("Dropping all tables!");
+	   
+	   try {
+		   DatabaseMetaData dbm = c.getMetaData();
+		   // check if table is there
+		   for (String t : tables) {
+			   ResultSet res = dbm.getTables(null, null, t, null);
+			   if (res.next()) {
+				   toDropTables.add(t);
+			   }
+		   }
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}  
+	   if (toDropTables.size() == 0) {
+		   return;
+	   }
+	   
 	   try
 	   {
 		   Statement stmt = null;
@@ -87,7 +105,7 @@ public class PostgreSQLJDBC {
 		   String sql = "DROP TABLE ";
 
 		   boolean isFirst = true;
-		   for(String curTable : tables)
+		   for(String curTable : toDropTables)
 		   {
 			   if(!isFirst)	sql += ", ";
 			   sql += curTable;
@@ -264,6 +282,17 @@ public class PostgreSQLJDBC {
 			                    " id_ori          CHAR(10)   , " +
 			                    " source          CHAR(10)) " ;
 			   }
+			   else if (t.equals("person_organization")) {
+				   System.out.println("Creating Table person_organization ...");
+			       sql = "CREATE TABLE person_organization " +
+			                    "(id SERIAL PRIMARY KEY     NOT NULL," +
+			                    " person_id          INTEGER REFERENCES person,  " +
+			                    " organization_id         INTEGER REFERENCES organization,  " +
+			                    " types          CHAR(100), " +
+			                    " direction      INTEGER , " +			        
+			                    " id_ori          CHAR(10)   , " +
+			                    " source          CHAR(10)) " ;
+			   }
 			   else if (t.equals("event_event")) {
 				   System.out.println("Creating Table event_event ...");
 			       sql = "CREATE TABLE event_event " +
@@ -378,8 +407,8 @@ public class PostgreSQLJDBC {
 				   System.out.println("Creating Table person_person ...");
 			       sql = "CREATE TABLE person_person " +
 			                    "(id SERIAL PRIMARY KEY     NOT NULL," +
-			                    " person_id          INTEGER REFERENCES person,  " +
-			                    " person_id         INTEGER REFERENCES person,  " +
+			                    " person_id_1          INTEGER REFERENCES person,  " +
+			                    " person_id_2         INTEGER REFERENCES person,  " +
 			                    " types          CHAR(100), " +
 			                    " direction      INTEGER,  " +			        
 			                    " id_ori          CHAR(10)   , " +
@@ -400,8 +429,8 @@ public class PostgreSQLJDBC {
 				   System.out.println("Creating Table organization_organization ...");
 			       sql = "CREATE TABLE organization_organization " +
 			                    "(id SERIAL PRIMARY KEY     NOT NULL," +
-			                    " organization_id          INTEGER REFERENCES organization,  " +
-			                    " organization_id         INTEGER REFERENCES organization,  " +
+			                    " organization_id_1          INTEGER REFERENCES organization,  " +
+			                    " organization_id_2         INTEGER REFERENCES organization,  " +
 			                    " types          CHAR(100), " +
 			                    " direction      INTEGER,  " +			        
 			                    " id_ori          CHAR(10)   , " +
@@ -422,8 +451,8 @@ public class PostgreSQLJDBC {
 				   System.out.println("Creating Table vehicle_vehicle ...");
 			       sql = "CREATE TABLE vehicle_vehicle " +
 			                    "(id SERIAL PRIMARY KEY     NOT NULL," +
-			                    " vehicle_id          INTEGER REFERENCES vehicle,  " +
-			                    " vehicle_id         INTEGER REFERENCES vehicle,  " +
+			                    " vehicle_id_1          INTEGER REFERENCES vehicle,  " +
+			                    " vehicle_id_2         INTEGER REFERENCES vehicle,  " +
 			                    " types          CHAR(100), " +
 			                    " direction      INTEGER,  " +			        
 			                    " id_ori          CHAR(10)   , " +
@@ -438,7 +467,7 @@ public class PostgreSQLJDBC {
 		   c.commit();
 	       stmt.close();		   
 	   } catch ( Exception e ) {
-	         System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+		   e.printStackTrace();
        }
    }
    
@@ -457,7 +486,7 @@ public class PostgreSQLJDBC {
 		   rs.close();
 		   stmt.close();
 	   } catch ( Exception e ) {
-	         System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+		   e.printStackTrace();
        }
 	   if (id == -1) {
 		   System.out.println(String.format("Error: Record not found for id_ori='%s' in Table '%s'", query, table));
@@ -513,6 +542,97 @@ public class PostgreSQLJDBC {
 				   sql = "INSERT INTO person_organization (person_id, organization_id, types, direction, id_ori, source) "
 			               + String.format("VALUES(%d, %d, '%s', %d, '%s', '%s');", person_id, organization_id, d.get("types"), d.get("direction"), d.get("id_ori"), d.get("source"));
 			   }
+			   else if (entity.equals("event_event")) {
+				   int event_id_1 = queryID(c, "", d.get("event_id_ori_1"));
+				   int event_id_2 = queryID(c, "event", d.get("event_id_ori_2"));
+				   sql = "INSERT INTO event_event (event_id_1, event_id_2, types, direction, id_ori, source) "
+			               + String.format("VALUES(%d, %d, '%s', %d, '%s', '%s');", event_id_1, event_id_2, d.get("types"), d.get("direction"), d.get("id_ori"), d.get("source"));
+			   }
+			   else if (entity.equals("facility_facility")) {
+				   int facility_id_1 = queryID(c, "facility", d.get("facility_id_ori_1"));
+				   int facility_id_2 = queryID(c, "facility", d.get("facility_id_ori"));
+				   sql = "INSERT INTO person_organization (facility_id_1, facility_id_2, types, direction, id_ori, source) "
+			               + String.format("VALUES(%d, %d, '%s', %d, '%s', '%s');", facility_id_1, facility_id_2, d.get("types"), d.get("direction"), d.get("id_ori"), d.get("source"));
+			   }
+			   else if (entity.equals("facility_location")) {
+				   int facility_id = queryID(c, "facility", d.get("facility_id_ori"));
+				   int location_id = queryID(c, "location", d.get("location_id_ori"));
+				   sql = "INSERT INTO facility_location (facility_id, location_id, types, direction, id_ori, source) "
+			               + String.format("VALUES(%d, %d, '%s', %d, '%s', '%s');", facility_id, location_id, d.get("types"), d.get("direction"), d.get("id_ori"), d.get("source"));
+			   }
+			   else if (entity.equals("facility_person")) {
+				   int facility_id = queryID(c, "facility", d.get("facility_id_ori"));
+				   int person_id = queryID(c, "person", d.get("person_id_ori"));
+				   sql = "INSERT INTO facility_person (facility_id, person_id, types, direction, id_ori, source) "
+			               + String.format("VALUES(%d, %d, '%s', %d, '%s', '%s');", facility_id, person_id, d.get("types"), d.get("direction"), d.get("id_ori"), d.get("source"));
+			   }
+			   else if (entity.equals("facility_organization")) {
+				   int facility_id = queryID(c, "facility", d.get("facility_id_ori"));
+				   int organization_id = queryID(c, "organization", d.get("organization_id_ori"));
+				   sql = "INSERT INTO facility_organization (facility_id, organization_id, types, direction, id_ori, source) "
+			               + String.format("VALUES(%d, %d, '%s', %d, '%s', '%s');", facility_id, organization_id, d.get("types"), d.get("direction"), d.get("id_ori"), d.get("source"));
+			   }
+			   else if (entity.equals("facility_vehicle")) {
+				   int facility_id = queryID(c, "facility", d.get("facility_id_ori"));
+				   int vehicle_id = queryID(c, "vehicle", d.get("vehicle_id_ori"));
+				   sql = "INSERT INTO facility_vehicle (facility_id, vehicle_id, types, direction, id_ori, source) "
+			               + String.format("VALUES(%d, %d, '%s', %d, '%s', '%s');", facility_id, vehicle_id, d.get("types"), d.get("direction"), d.get("id_ori"), d.get("source"));
+			   }
+			   else if (entity.equals("location_location")) {
+				   int location_id_1 = queryID(c, "location", d.get("location_id_ori_1"));
+				   int location_id_2 = queryID(c, "location", d.get("location_id_ori_2"));
+				   sql = "INSERT INTO location_location (location_id_1, location_id_2, types, direction, id_ori, source) "
+			               + String.format("VALUES(%d, %d, '%s', %d, '%s', '%s');", location_id_1, location_id_2, d.get("types"), d.get("direction"), d.get("id_ori"), d.get("source"));
+			   }
+			   else if (entity.equals("location_person")) {
+				   int location_id = queryID(c, "location", d.get("location_id_ori"));
+				   int person_id = queryID(c, "person", d.get("person_id_ori"));
+				   sql = "INSERT INTO location_person (location_id, person_id, types, direction, id_ori, source) "
+			               + String.format("VALUES(%d, %d, '%s', %d, '%s', '%s');", location_id, person_id, d.get("types"), d.get("direction"), d.get("id_ori"), d.get("source"));
+			   }
+			   else if (entity.equals("location_organization")) {
+				   int location_id = queryID(c, "location", d.get("location_id_ori"));
+				   int organization_id = queryID(c, "organization", d.get("organization_id_ori"));
+				   sql = "INSERT INTO location_organization (location_id, organization_id, types, direction, id_ori, source) "
+			               + String.format("VALUES(%d, %d, '%s', %d, '%s', '%s');", location_id, organization_id, d.get("types"), d.get("direction"), d.get("id_ori"), d.get("source"));
+			   }
+			   else if (entity.equals("location_vehicle")) {
+				   int location_id = queryID(c, "location", d.get("location_id_ori"));
+				   int vehicle_id = queryID(c, "vehicle", d.get("vehicle_id_ori"));
+				   sql = "INSERT INTO location_vehicle (location_id, vehicle_id, types, direction, id_ori, source) "
+			               + String.format("VALUES(%d, %d, '%s', %d, '%s', '%s');", location_id, vehicle_id, d.get("types"), d.get("direction"), d.get("id_ori"), d.get("source"));
+			   }
+			   else if (entity.equals("person_person")) {
+				   int person_id_1 = queryID(c, "person", d.get("person_id_ori_1"));
+				   int person_id_2 = queryID(c, "person", d.get("person_id_ori_2"));
+				   sql = "INSERT INTO person_person (person_id_1, person_id_2, types, direction, id_ori, source) "
+			               + String.format("VALUES(%d, %d, '%s', %d, '%s', '%s');", person_id_1, person_id_2, d.get("types"), d.get("direction"), d.get("id_ori"), d.get("source"));
+			   }
+			   else if (entity.equals("person_vehicle")) {
+				   int person_id = queryID(c, "person", d.get("person_id_ori"));
+				   int vehicle_id = queryID(c, "vehicle", d.get("vehicle_id_ori"));
+				   sql = "INSERT INTO person_vehicle (person_id, vehicle_id, types, direction, id_ori, source) "
+			               + String.format("VALUES(%d, %d, '%s', %d, '%s', '%s');", person_id, vehicle_id, d.get("types"), d.get("direction"), d.get("id_ori"), d.get("source"));
+			   }
+			   else if (entity.equals("organization_organization")) {
+				   int organization_id_1 = queryID(c, "organization", d.get("organization_id_ori_1"));
+				   int organization_id_2 = queryID(c, "organization", d.get("organization_id_ori_2"));
+				   sql = "INSERT INTO organization_organization (organization_id_1, organization_id_2, types, direction, id_ori, source) "
+			               + String.format("VALUES(%d, %d, '%s', %d, '%s', '%s');", organization_id_1, organization_id_2, d.get("types"), d.get("direction"), d.get("id_ori"), d.get("source"));
+			   }
+			   else if (entity.equals("organization_vehicle")) {
+				   int organization_id = queryID(c, "organization", d.get("organization_id_ori"));
+				   int vehicle_id = queryID(c, "vehicle", d.get("vehicle_id_ori"));
+				   sql = "INSERT INTO organization_vehicle (organization_id, vehicle_id, types, direction, id_ori, source) "
+			               + String.format("VALUES(%d, %d, '%s', %d, '%s', '%s');", organization_id, vehicle_id, d.get("types"), d.get("direction"), d.get("id_ori"), d.get("source"));
+			   }
+			   else if (entity.equals("vehicle_vehicle")) {
+				   int vehicle_id_1 = queryID(c, "vehicle", d.get("vehicle_id_ori_1"));
+				   int vehicle_id_2 = queryID(c, "vehicle", d.get("vehicle_id_ori_2"));
+				   sql = "INSERT INTO vehicle_vehicle (vehicle_id_1, vehicle_id_2, types, direction, id_ori, source) "
+			               + String.format("VALUES(%d, %d, '%s', %d, '%s', '%s');", vehicle_id_1, vehicle_id_2, d.get("types"), d.get("direction"), d.get("id_ori"), d.get("source"));
+			   }
+			   
 			   if (sql != null) {
 				   System.out.println(sql);
 				   stmt.executeUpdate(sql);
@@ -523,7 +643,6 @@ public class PostgreSQLJDBC {
 		   stmt.close();
 		   c.commit();
 	   } catch ( Exception e ) {
-	         System.err.println( e.getClass().getName()+": "+ e.getMessage() );
 	         e.printStackTrace();
        }
    }
@@ -596,7 +715,6 @@ public class PostgreSQLJDBC {
 		   stmt.close();
 		   c.commit();
 	   } catch ( Exception e ) {
-	         System.err.println( e.getClass().getName()+": "+ e.getMessage() );
 	         e.printStackTrace();
        }
    }
@@ -609,7 +727,6 @@ public class PostgreSQLJDBC {
 	            username, password);  // username and password
 	      } catch (Exception e) {
 	         e.printStackTrace();
-	         System.err.println(e.getClass().getName()+": "+e.getMessage());
 	         System.exit(0);
 	      }
 	   System.out.println("Opened database successfully");   
@@ -621,7 +738,7 @@ public class PostgreSQLJDBC {
 		   c.close();	
 		   System.out.println("Close Database");
 	   } catch (Exception e) {
-		   System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+		   e.printStackTrace();
 	       System.exit(0);
 	   }
    }
